ef8d0f12771bc3393d46ebe3336388eed1777477
RobertSolovev39 <RobertSolovev39@gmail.com>
2022-06-10 17:00:36 +0700

    code reduced

--- source/utilities/to_utc.py
+++ source/utilities/to_utc.py
@@ -1,9 +1,9 @@
 from datetime import datetime, timedelta
 
-utc = datetime.utcnow()
-now = datetime.now()
-
-difference = timedelta(hours=now.hour - utc.hour, minutes=now.minute - utc.minute)
+difference = timedelta(
+  hours=(now := datetime.now()).hour - (utc := datetime.utcnow()).hour,
+  minutes=now.minute - utc.minute
+)
 
 
 def to_utc(date: datetime) -> datetime:
