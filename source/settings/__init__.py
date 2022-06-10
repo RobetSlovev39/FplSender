eef201f0d95d654da3a9abca62935f7025bc4e6e
RobertSolovev39 <RobertSolovev39@gmail.com>
2022-06-10 16:51:36 +0700

    mail_at

--- source/settings/__init__.py
+++ source/settings/__init__.py
@@ -1,2 +1,3 @@
+from .settings import MAIL_AT
 from .telegram_bot import telegram_bot, authenticate, close_session
 from .dispatcher import dispatcher
--- source/settings/settings.py
+++ source/settings/settings.py
@@ -1,9 +1,9 @@
 from pathlib import Path
-from ..utilities import get_env_var, validate_time
+from ..utilities import get_env_var, validate_time, to_utc
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MAIL_AT = validate_time(get_env_var('MAIL_AT'))
+MAIL_AT = to_utc(validate_time(get_env_var('MAIL_AT')))
 
 TG_TOKEN = get_env_var('TG_TOKEN')
 ADMIN_ID = int(get_env_var('ADMIN_ID'))
