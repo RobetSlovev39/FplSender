626b49ca8940d78e622e920b1cc54dc672f92102
RobertSolovev39 <RobertSolovev39@gmail.com>
2022-06-10 17:02:51 +0700

    space

--- source/settings/dispatcher.py
+++ source/settings/dispatcher.py
@@ -3,6 +3,5 @@ from .telegram_bot import telegram_bot
 from aiogram import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-
 storage = MemoryStorage()
 dispatcher = Dispatcher(telegram_bot, storage=storage)
