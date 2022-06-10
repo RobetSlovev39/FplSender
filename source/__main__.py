744d119eaa7ccadde84d355acce6b17b61a6a5df
RobertSolovev39 <RobertSolovev39@gmail.com>
2022-06-10 16:52:21 +0700

    background run

--- source/__main__.py
+++ source/__main__.py
@@ -1,11 +1,15 @@
 import asyncio
 from aiogram.utils.exceptions import NetworkError
 
+from .background import background
 from .settings import authenticate, dispatcher, close_session
+
 
 
 async def main() -> None:
   me = await authenticate()
+
+  asyncio.create_task(background())
 
   print('Polling started as', me)
   await dispatcher.start_polling()
