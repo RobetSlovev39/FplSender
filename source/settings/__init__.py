e936f90ae6060ffe7c54789ef8ccdf8ff03b4bf1
RobertSolovev39 <RobertSolovev39@gmail.com>
2022-07-19 09:48:42 +0700

    smtp

--- source/settings/__init__.py
+++ source/settings/__init__.py
@@ -1,4 +1,4 @@
-from .settings import BASE_DIR
+from .settings import BASE_DIR, MAIL_HOSTNAME, MAIL_SENDER, MAIL_PASSWORD, MAIL_PORT, MAIL_USE_TLS
 from .telegram_bot import telegram_bot, authenticate, close_session
 from .dispatcher import dispatcher
-from .configuration import configuration, recipients
+from .configuration import configuration, recipients, channels
--- source/settings/settings.py
+++ source/settings/settings.py
@@ -5,3 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 TG_TOKEN = get_env_var('TG_TOKEN')
 ADMIN_ID = int(get_env_var('ADMIN_ID'))
+
+MAIL_HOSTNAME = get_env_var('MAIL_HOSTNAME')
+MAIL_SENDER = get_env_var('MAIL_SENDER')
+MAIL_PASSWORD = get_env_var('MAIL_PASSWORD')
+MAIL_PORT = get_env_var('MAIL_PORT', 587)
+MAIL_USE_TLS = bool(get_env_var('MAIL_USE_TLS', 0))
